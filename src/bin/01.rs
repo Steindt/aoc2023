bc7747980a99c134b81c546354533fb0f5facc33
stein <tairstenen@gmail.com>
2024-06-01 12:47:00 +0200

    Initial commit

---
+++ src/bin/01.rs
@@ -0,0 +1,10 @@
+use std::fs;
+
+fn main() {
+  let input = fs::read_to_string("../../data/examples/01b").expect("Couldn't find input file");
+  for line in input.lines() {
+    let mut a: &str = "";
+    for char in line.chars() {
+    }
+  }
+}
